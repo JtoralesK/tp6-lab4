66807127520431683e2d3a3268949600191acbdb
Agustin Sbernini <fliasbernini@hotmail.com>
2023-10-05 23:34:10 -0300

    Modificacion del toString de persona

--- src/entidad/Persona.java
+++ src/entidad/Persona.java
@@ -38,7 +38,7 @@ public class Persona {
 	
 	@Override
 	public String toString() {
-		return "Persona [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + "]";
+		return apellido + ", " + nombre + " con DNI: " + dni;
 	}
 
 }
